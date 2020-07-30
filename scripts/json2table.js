ee482d148ae8871d1dc7fca3cbc95d7f3d9cdc03
fanlj <fanlj@autoai.com>
2020-07-30 18:13:47 +0800

    修改json转表格方法

--- scripts/json2table.js
+++ scripts/json2table.js
@@ -33,98 +33,66 @@ export default (function(){
         });
         return _allSubs;
     }
-    return {
-        render () {
-
+    function renderHead(obj){
+        if(({}).toString.call(obj) != '[object Object]') return null; 
+        let head = '', _col, _label, _colspan, _width;
+        for(let key in obj){
+            if(typeof(obj[key]) != 'object') continue;
+            _col = '<col>'.repeat(tableInfo.colCount -1);
+            break;
         }
+        for(let key in obj){
+            _label = typeof(obj[key]) == 'string' ? obj[key] : obj[key].name;
+            _width = typeof(obj[key]) == 'object' ? (obj[key].width || '') : '';
+            if(!!_width){
+                _width = `width:${_width};`;
+            }
+            if(_col){
+                _col += `<col style="${_width}">`;
+            }
+            _colspan = key == 'fieldName' ? ` colspan="${obj[key].colspan || tableInfo.colCount}"` : '';
+            head += `<th${_colspan}>${_label}</th>`;
+        }
+        return !!_col ? `<colgroup>${_col}</colgroup><tr>${head}</tr>` : `<tr>${head}</tr>`;
     }
-})();
-
-
-var tableModule = {
-    tableInfo: {
-        colCount: 1,
-        rowCount: {}
-    },
-    initTableInfo: function(rows, level){
-        let _rows = rows.filter(r => r.subs && r.subs.length > 0);
-        if(_rows.length == 0){ return 0; }
-        level++;
-        tableModule.tableInfo.colCount = Math.max(level, tableModule.tableInfo.colCount);
-        let _key, _count, _allSubs = 0, _subs = 0;
-        _rows.forEach(s => {
-            _count = s.subs.length;
-            _subs = tableModule.initTableInfo(s.subs, level);
-            _count += _subs;
-            _allSubs += (_subs + s.subs.length);
-            _key = `${s.subs[0].fieldName}_${level}`;
-            tableModule.tableInfo.rowCount[_key] = _count;
-        });
-        return _allSubs;
-    },
-    createRowHtml: function(row, level, idx){
-        if(({}).toString.call(row) != '[object Object]'){ return null; }
+    function createRowHtml (row, level, idx){
+        if(({}).toString.call(row) != '[object Object]') return null;
         let _html = '', _blank = '', _key = `${row.fieldName}_${level}`;
-        let colspan = tableModule.tableInfo.colCount - level + 1;
+        let colspan = tableInfo.colCount - level + 1;
         colspan = colspan > 1 ? ` colspan="${colspan}"` : '';
-        let rowspan = tableModule.tableInfo.rowCount[_key] || 0;
-    
+        let rowspan = tableInfo.rowCount[_key] || 0;
         for (let key in row) {
-            if(key == 'subs'){ continue; }
+            if(key == tableConf.nestedField) continue;
             _html += (key == 'fieldName') ? `<td ${colspan}><p>${row[key]}</p></td>` : `<td><p>${row[key]}</p></td>`;
         }
         _blank = idx == 0 && level > 1 ? (rowspan > 1 ? `<td rowspan="${rowspan}"></td>` : rowspan > 0 ? '<td rowspan></td>' : '') : '';
         let val = `<tr>${_blank}${_html}</tr>`;
         return val;
-    },
-    renderRow: function(rows, level){
+    }
+    function renderRow (rows, level){
         let html = '', result = '';
         rows.forEach((row, idx) => {
             _level = level;
             if(!row.subs || row.subs.length == 0){
-                result = tableModule.createRowHtml(row, _level, idx);
+                result = createRowHtml(row, _level, idx);
             }else{
-                result = tableModule.createRowHtml(row, level, 0);
+                result = createRowHtml(row, level, 0);
                 _level = level + 1;
-                result += tableModule.renderRow(row.subs, _level);
+                result += renderRow(row.subs, _level);
             }
             html += result;
         });
         return html;
-    },
-    renderHead: function(obj){
-        if(({}).toString.call(obj) != '[object Object]'){ return null; }
-        let head = '', _col = '', _label, _colspan, _width, _createCols = false;
-        for(let key in obj){
-            if(typeof(obj[key]) != 'object'){ continue; }
-            _createCols = true;
-            break;
-        }
-        if(_createCols){
-            for(let i = 0; i < tableModule.tableInfo.colCount -1; i++){ _col += '<col>'; }
-        }
-        for(let key in obj){
-            _label = typeof(obj[key]) == 'string' ? obj[key] : obj[key].name;
-            _width = typeof(obj[key]) == 'object' ? (obj[key].width || '') : '';
-            if(!!_width){ _width = `width:${_width};`; }
-            if(_createCols){ _col += `<col style="${_width}">`; }
-            _colspan = key == 'fieldName' ? ` colspan="${obj[key].colspan || tableModule.tableInfo.colCount}"` : '';
-            head += `<th${_colspan}>${_label}</th>`;
-        }
-        if(!!_col){ _col = `<colgroup>${_col}</colgroup>`; }
-        return `${_col}<tr>${head}</tr>`;
     }
-}
-
-// 自定义指令处理函数（函数名即自定义指令名）
-export default {
-    table_render: function(html, data){
-        if(({}).toString.call(data) != '[object Object]'){ return null; }
-        tableModule.tableInfo.colCount = 1;
-        tableModule.tableInfo.rowCount = {};
-        tableModule.initTableInfo(data.table, 1);
-        let thead = tableModule.renderHead(data.tableHead);
-        let tbody = tableModule.renderRow(data.table, 1);
+    return function render (data, config) {
+        if(({}).toString.call(data) != '[object Object]'){
+            throw Error('parameter "data" must be an Object!');
+        }
+        tableConf = Object.assign(tableConf, config);
+        tableInfo = { colCount: 1, rowCount: {} };
+        initStructureInfo(data.table, 1);
+        let thead = renderHead(data.tableHead);
+        let tbody = renderRow(data.table, 1);
         return `<table cellspacing="0">${thead}${tbody}</table>`;
     }
-}
+})();
