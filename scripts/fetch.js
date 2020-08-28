997de85fede60b3222a5f9486427eceb16666d2a
fanlj <fanlj@autoai.com>
2020-08-28 10:13:01 +0800

    修改取消请求的方法

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -2,21 +2,20 @@
  * 配置axios请求相关
  * fanlinjun
  **/
-
 import axios from 'axios';
 import qs from 'querystring';
-const source = axios.CancelToken.source();
+const reqStack = [];
 
 // 空函数，不会执行任何操作
 function noop(){}
 
-// 可自定义的拦截器钩子函数的名称集合，钩子函数可用于axios实例发送请求前后调用
+// 可自定义的拦截器钩子函数的名称集合，钩子函数用于向axios实例中注入对应拦截器
 const hookFns = ['before', 'success', 'error'];
 
 // 自定义axios拦截器钩子函数集合
 let axiosHooks = {};
 
-// 初始化axios自定义拦截器钩子函数，初始函数均为noop
+// 自动初始化axios自定义拦截器钩子函数，所有函数均为noop
 hookFns.forEach(m => {
     axiosHooks[m] = noop;
 });
@@ -24,7 +23,7 @@ hookFns.forEach(m => {
 // 设置默认请求头及超时时间
 axios.defaults.headers.get['Cache-Control'] = 'no-cache';
 axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.timeout = 60000;
 
@@ -42,7 +41,7 @@ function injectHook(instance, options) {
         _before(conf);
         return conf;
     }, err => {
-        _error();
+        _error(err);
         return Promise.reject(err);
     });
 
@@ -52,7 +51,7 @@ function injectHook(instance, options) {
         let _res = _success(res);
         return (_res || res);
     }, err => {
-        _error();
+        _error(err);
         return Promise.reject(err);
     });
 }
@@ -74,9 +73,9 @@ function initRequestHooks(options) {
  * @param {Object} options axios实例选项
  * @returns {Axios} Axios实例
  */
-function getFetch(options){
+function getFetcher(options){
     let config = {
-        cancelToken: source.token,
+        cancelToken: new axios.CancelToken(c => reqStack.push(c)),
     };
     options = Object.assign({}, options);
     if(/^get$/i.test(options.method)){
@@ -96,15 +95,11 @@ function getFetch(options){
 
 /**
  * fetch对象，有get、post、put和delete四个方法用于对应http请求
- * all和spread为axios对应的方法，用于多个请求
  */
-let fetch = {
-    all: axios.all,
-    spread: axios.spread
-};
+let fetch = {};
 ['get', 'post', 'put', 'delete'].forEach(m => {
     fetch[m] = function(url, pms, options){
-        let instance = getFetch(Object.assign({}, options, { method: m }));
+        let instance = getFetcher(Object.assign({}, options, { method: m }));
         if(m === 'get' && !pms.params){
             pms = { params: pms };
         }
@@ -115,13 +110,14 @@ let fetch = {
 /**
  * @description 取消请求
  */
-function cancelFetches() {
-     source.cancel('Operation canceled!');
+function cancelRequests() {
+    reqStack.forEach(cancel => cancel());
+    reqStack.splice(0);
 }
 
 export {
     fetch,
     initRequestHooks,
-    getFetch,
-    cancelFetches
+    getFetcher,
+    cancelRequests
 }
